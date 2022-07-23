2fc62c834b277be27c936b0372c41abf8b46f310
metkeosambo <55055063+metkeosambo@users.noreply.github.com>
2022-07-23 08:21:24 +0700

    update

--- src/main/java/com/example/app_api/service/UserServiceImpl.java
+++ src/main/java/com/example/app_api/service/UserServiceImpl.java
@@ -15,37 +15,49 @@ import com.example.app_api.util.LoggerConfig;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
 	private final PlatformTransactionManager transactionManager;
 	public UserServiceImpl(PlatformTransactionManager transactionManager) {
 	    this.transactionManager = transactionManager;
 	  }
+	
 	@Autowired
 	UserDao userDao;
 	
 	@Override
 	public User createUser(User user) {
+		
 		TransactionStatus txStatus =
 		        transactionManager.getTransaction(new DefaultTransactionDefinition());
-		//return userDao.createUser(user);
+
 		User u = new User();
+		
 		try {
 			  u = userDao.createUser(user);
+			
 		    } catch (Exception e) {
+			
 		      LoggerConfig.setError(UserServiceImpl.class.toString(), e.getMessage());
 		      transactionManager.rollback(txStatus);
 		      throw e;
+			
 		    }
+		
 		    transactionManager.commit(txStatus);
 		    return u;	    
 	}
 
 	@Override
 	public User getUser(Long userId) {
+		
 		return userDao.getUser(userId);
+		
 	}
 
 	@Override
 	public List<User> getAllUser() {
+		
 		return userDao.getAllUser();
+		
 	}
 }
